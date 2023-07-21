fc695eb03856a53645125e4d7febf676987f21ad
vishalshetty104 <vishalshetty104@gmail.com>
2023-07-21 10:34:49 +0530

    added base files

---
+++ src/main/java/vishalshetty104/feedbackAPI/main/MainApplication.java
@@ -0,0 +1,13 @@
+package vishalshetty104.feedbackAPI.main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MainApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MainApplication.class, args);
+	}
+
+}
---
+++ src/test/java/vishalshetty104/feedbackAPI/main/MainApplicationTests.java
@@ -0,0 +1,13 @@
+package vishalshetty104.feedbackAPI.main;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MainApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
